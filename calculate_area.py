317fee5e9bc0778218074c5c185d845640321591
eMobilis <njremmanuel235@gmail.com>
2023-06-26 09:20:25 +0300

    calculated the area

---
+++ calculate_area.py
@@ -0,0 +1,11 @@
+#enable user to enter input
+height = input("enter height")
+width = input("enter width")
+
+#convert two variables into intergers
+height = int(height)
+width = int(width)
+
+#perform calculation
+result = height * width
+print("area is", result)
