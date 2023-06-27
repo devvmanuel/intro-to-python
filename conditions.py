b49b044ecbcfe47fc2772e59b69966df367c2122
eMobilis <njremmanuel235@gmail.com>
2023-06-27 09:05:38 +0300

    entered a simple score system

---
+++ conditions.py
@@ -0,0 +1,8 @@
+a = input("enter first number")
+b = input("enter second number")
+
+if a > b:
+    print("true")
+
+else:
+    print("false")
---
+++ score system.py
@@ -0,0 +1,11 @@
+marks = int(input("enter marks"))
+if marks < 40:
+    print("E")
+elif marks < 50:
+    print("D")
+elif marks < 60:
+    print("C")
+elif marks < 70:
+    print("B")
+else:
+    print("A")
