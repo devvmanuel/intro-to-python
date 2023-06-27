302f899156c3f03bfe75c04e04e9eddaf05d6ba8
eMobilis <njremmanuel235@gmail.com>
2023-06-27 09:46:37 +0300

    betting strategy

---
+++ betting.py
@@ -0,0 +1,13 @@
+reward = int(input("Enter number"))
+
+if reward == 1:
+    print("won")
+elif reward == 2:
+    print("2mz")
+elif reward == 3:
+    print("ps3")
+elif reward == 4:
+    print("ps4")
+elif reward == 5:
+    print("carz")
+
