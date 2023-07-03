4caf3ac936430166e564ce9597154b2e92a0750a
eMobilis <njremmanuel235@gmail.com>
2023-07-03 10:26:41 +0300

    intro

---
+++ functions/functions with params.py
@@ -0,0 +1,14 @@
+def addnums(x ,y):
+    result = x + y
+    print(result)
+
+
+addnums( 100 , 15)
+
+
+
+def showname(first,last):
+    print(first,last)
+
+
+showname(input("enter first name"), input("enter last name"))
