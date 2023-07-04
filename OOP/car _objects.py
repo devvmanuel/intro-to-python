2714b8bb71845bba2ad30317826acfdeb4095b93
eMobilis <njremmanuel235@gmail.com>
2023-07-04 09:37:33 +0300

    updated python files

---
+++ OOP/car _objects.py
@@ -0,0 +1,11 @@
+from car import Car
+
+owner1 = Car("toyota","vitz","kim","black")
+owner2 = Car("toyota","vitz","joe","black")
+owner3 = Car("toyota","vitz","vaz","black")
+owner4 = Car("toyota","vitz","pue","black")
+
+print(owner1.make,owner1.model,owner1.owner,owner1.color)
+print(owner2.make,owner2.model,owner2.owner,owner2.color)
+print(owner3.make,owner3.model,owner3.owner,owner3.color)
+print(owner4.make,owner4.model,owner4.owner,owner4.color)
---
+++ modules/module2.py
@@ -0,0 +1,6 @@
+from module1 import *
+
+
+print(addtwonums(15,787))
+print(subtwonums(450,230))
+print(multtwonums(34,233))
