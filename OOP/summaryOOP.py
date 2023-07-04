41f011f968e98c4d4562d44b3975d9de4e16464b
eMobilis <njremmanuel235@gmail.com>
2023-07-04 10:22:28 +0300

    summary

---
+++ OOP/summaryOOP.py
@@ -0,0 +1,9 @@
+#OOP is a paradigm / way of writing code that makes it more re- useable and efficient
+#we have classes and objects
+#Class is like a blueprint
+#Objects = instance of a class
+
+#inheritence
+#polymorphisim
+#Abstraction
+#Encapsulation
