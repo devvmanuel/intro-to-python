31f03f48fd6c9499dabaf293a8ea3128c8092009
eMobilis <njremmanuel235@gmail.com>
2023-07-04 10:05:17 +0300

    house objects

---
+++ OOP/house objects.py
@@ -0,0 +1,16 @@
+from house import House
+
+owner1 = House("mansion","blue","max","woodley")
+owner2 = House("mansion","red","sue","woodley")
+owner3 = House("mansion","black","grace","woodley")
+owner4 = House("mansion","yellow","mia","woodley")
+owner5 = House("mansion","green","foden","woodley")
+
+
+
+
+print(owner1.model,owner1.color,owner1.owner,owner1.estate)
+print(owner2.model,owner2.color,owner2.owner,owner2.estate)
+print(owner3.model,owner3.color,owner3.owner,owner3.estate)
+print(owner4.model,owner4.color,owner4.owner,owner4.estate)
+print(owner5.model,owner5.color,owner5.owner,owner5.estate)
---
+++ OOP/house.py
@@ -0,0 +1,8 @@
+
+
+class House:
+    def __init__(self,model,color,owner,estate):
+        self.model = model
+        self.color = color
+        self.owner = owner
+        self.estate = estate
